453e23aeaa7ee4643c3ed63e1fde02e66ed03084
Ashtyn19 <Ashtyn_binder@gooroo.com.au>
2025-06-10 21:14:49 +1000

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -19,6 +19,6 @@ local Games = {
   [85896571713843] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Bubble%20Gum%20Simulator%20INFINITY.lua", -- Bubble Gum Simulator INFINITY
   [126884695634066] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Grow%20a%20Garden.lua", -- Grow a Garden
   [91867617264223] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Grow%20a%20Garden.lua", -- Grow a Garden 1
-}
+loadstring(game:HttpGet('https://raw.githubusercontent.com/m00ndiety/Grow-a-garden/refs/heads/main/Grow-A-fkin-Garden.txt'))()-- Discord https://discord.gg/wZ4hBXSrxY
 
 return Games
